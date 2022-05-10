3ab42d7f9fe35f479d661655f3974455bfaffb22
Duc Minh <taducminh2004@gmail.com>
2022-05-10 22:08:00 +0700

    init

---
+++ src/components/Navbar.js
@@ -0,0 +1,29 @@
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../assets/1200px-Shop.svg.png";
+import "../styles/Navbar.css";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <img className="logo" src={logo} alt="logo" />
+      <ul className="menu">
+        <li className="menu-item">
+          <Link to="/">Home</Link>
+        </li>
+        <li className="menu-item pre-last">
+          <Link to="/product">Product</Link>
+        </li>
+        <li className="menu-item">
+          <Link to="/signin">
+            <FontAwesomeIcon icon={faUser} />
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
