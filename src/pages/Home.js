f44d84b000879e99cede8ba6cb0c27423a1f7d84
Duc Minh <taducminh2004@gmail.com>
2022-05-14 20:44:41 +0700

    add banner

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React from "react";
+import banner from "../assets/istockphoto-823256052-170667a.jpg";
+import "../styles/Home.css"
 
 const Home = () => {
   return (
-    <div>
-      Home
+    <div className="home">
+      <img className="banner" src={banner} alt="banner" />
     </div>
   );
-}
+};
 
 export default Home;
