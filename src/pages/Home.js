affd8509fb86ed6272a25aa2d71a67a7c2b7a31b
Duc Minh <taducminh2004@gmail.com>
2022-05-14 21:58:56 +0700

    add footer code

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,16 @@
 import React from "react";
 import banner from "../assets/istockphoto-823256052-170667a.jpg";
-import "../styles/Home.css"
+import ProductCard from "../components/ProductCard";
+import "../styles/Home.css";
 
 const Home = () => {
   return (
     <div className="home">
       <img className="banner" src={banner} alt="banner" />
+      <div>
+        <h3>Lastest Product</h3>
+        <ProductCard />
+      </div>
     </div>
   );
 };
